6aa5c30cb251bb20f4eac237e750d2f0472f7d8c
Sankar8098 <113809269+Sankar8098@users.noreply.github.com>
2023-10-08 07:59:10 +0000

    Update main.py

--- main.py
+++ main.py
@@ -64,11 +64,17 @@ def progress(current, total, message, type):
 
 # start command
 @bot.on_message(filters.command(["start"]))
-def send_start(client: pyrogram.client.Client, message: pyrogram.types.messages_and_media.message.Message):
-	bot.send_message(message.chat.id, f"__👋 Hi **{message.from_user.mention}**, I am Save Restricted Bot, I can send you restricted content by it's post link__",
-	reply_markup=InlineKeyboardMarkup([[ InlineKeyboardButton("🌐 Source Code", url="https://github.com/bipinkrish/Save-Restricted-Bot")]]), reply_to_message_id=message.id)
+async def start(client,message):
 
-
+	 await message.reply_text(text =f"""
+	Hello {wish} {message.from_user.first_name }
+	__I am file renamer bot, Please sent any telegram 
+	**Document Or Video** and enter new filename to rename it__
+	""",reply_to_message_id = message.id ,  
+	reply_markup=InlineKeyboardMarkup(
+	 [[ InlineKeyboardButton("Support 🇮🇳" ,url="https://t.me/lntechnical") ], 
+	[InlineKeyboardButton("Subscribe 🧐", url="https://youtube.com/c/LNtechnical")
+	 
 @bot.on_message(filters.text)
 def save(client: pyrogram.client.Client, message: pyrogram.types.messages_and_media.message.Message):
 
